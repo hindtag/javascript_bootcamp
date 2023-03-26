a191e28a7d70c52b973878fee5c3dcb059181d6b
hindtag <mbnvargas>
2023-03-27 02:11:17 +0800

    Learning pattern completed

--- patterns/README.md
+++ patterns/README.md
@@ -1,5 +1,11 @@
 # How to Approach
 
+1. Count how many no. of rows.
+2. Identify how many columns are there in each rows.
+3. What you need to print.
+
+## Steps
+
 1. Run the outer for loop for how many number of times you are having the lines.
    - No. of lines is equal to no. of rows
    - No. of times outer loop will run
@@ -9,9 +15,12 @@
 5. Print the space for the outer loop
 6. Code the inner loop for the needed column
 7. Print the symbol for the inner loop
-8. Console log/Print
+8. Print new line
 
 ## Note:
 
 - Outer for loop are for rows
 - Inner for loop are for columns
+
+Note: Continue on pattern 3
+https://www.tutorialstonight.com/js/javascript-star-pattern
--- patterns/main.js
+++ patterns/main.js
@@ -1,4 +1,35 @@
-// Detailed explanation
+/* || Patter no 1
+
+*****
+*****
+*****
+*****
+*****
+
+// Step 1 - Initiate
+let numberOfRows = 5;
+let numberOfColumn = 5;
+
+// Step 2 - Run the external loop
+for (let row = 1; row <= numberOfRows; row++) {
+	// Step 3 - Print the symbol
+	let symbol = '';
+	// Step 4 - Run the internal loop
+	for (let col = 1; col <= numberOfColumn; col++) {
+		symbol += '*';
+	}
+	console.log(symbol);
+}
+
+// - End no. 1 */
+
+/* || Pattern no 2
+
+*
+**
+***
+****
+*****
 
 // Step 1 - Initiate
 let numberOfRows = 5;
@@ -11,5 +42,152 @@ for (let row = 1; row <= numberOfRows; row++) {
 		// Step 5 - Print the symbol
 		symbol += '*';
 	}
+	// Print new line
 	console.log(symbol);
 }
+
+
+// - End no. 2 */
+
+/* || Pattern no 3
+
+*****
+****
+***
+**
+*
+
+
+// Initiate
+// Step 1 - Initiate
+let numberOfRows = 5;
+// Step 2 - Run the external loop
+for (let row = 1; row <= numberOfRows; row++) {
+	// Step 3 - Print the space
+	let symbol = '';
+	// Step 4 - Run the internal loop
+	for (let col = 1; col <= numberOfRows - row + 1; col++) {
+		// Step 5 - Print the symbol
+		symbol += '*';
+	}
+	// Print new line
+	console.log(symbol);
+}
+
+// - End no. 3 */
+
+/* || Pattern no 4
+
+// Step 1 - Initiate
+let numberOfRows = 5;
+// Step 2 - Run the external loop
+for (let row = 1; row <= numberOfRows; row++) {
+	// Step 3 - Print the space
+	let symbol = '';
+	// Step 4 - Run the internal loop
+	for (let col = 1; col <= row; col++) {
+		// Step 5 - Print the symbol
+		symbol += col;
+	}
+	// Print new line
+	console.log(symbol);
+}
+
+// - End no. 4 */
+
+/* || Pattern no 5
+
+*
+**
+***
+****
+*****
+*****
+****
+***
+**
+*
+
+// Step 1 - Initiate
+let numberOfRows = 5;
+// Step 2 - Run the external loop
+for (let row = 1; row <= numberOfRows; row++) {
+	// Step 3 - Print the space
+	let symbol = '';
+	// Step 4 - Run the internal loop
+	for (let col = 1; col <= row; col++) {
+		// Step 5 - Print the symbol
+		symbol += '*';
+	}
+	// Print new line
+	console.log(symbol);
+}
+
+// Step 6 - Start decreasing
+for (let row = numberOfRows; row >= 1; row--) {
+	// Step 7 - Print the space
+	let symbol = '';
+	// Step 8 - Run the internal loop
+	for (let col = 1; col <= row; col++) {
+		// Step 9 - Print the symbol
+		symbol += '*';
+	}
+	// Print new line
+	console.log(symbol);
+}
+
+// - End no. 5 */
+
+/* || Pattern no 6
+
+'*****';
+'*   *';
+'*   *';
+'*   *';
+'*****';
+
+// Step 1 - Initiate
+let numberOfRows = 5;
+
+// Step 2 - Run the external loop
+for (let row = 1; row <= numberOfRows; row++) {
+	// Step 3 - Print the symbol
+	let symbol = '';
+	// Step 4 - Run the internal loop
+	for (let col = 1; col <= numberOfRows; col++) {
+		if (row == 1 || row == numberOfRows || col == 1 || col == numberOfRows) {
+			symbol += '*';
+		} else {
+			symbol += ' ';
+		}
+	}
+	console.log(symbol);
+}
+
+// - End no. 6 */
+
+/* || Pattern no 7
+
+    *
+   **
+  ***
+ ****
+*****
+
+let height = 5;
+let symbol = '';
+
+for (let i = 1; i <= height; i++) {
+	// printing spaces
+	for (let j = 0; j < height - i; j++) {
+		symbol += ' ';
+	}
+	// printing star
+	for (let k = 0; k < i; k++) {
+		symbol += '*';
+	}
+	symbol += '\n';
+}
+console.log(symbol);
+
+// - End no. 7 */
