fd33b52220a9a0d448b066902f817eb0a662b22d
hindtag <mbnvargas>
2023-03-31 17:30:09 +0800

    JS ebook page 64

---
+++ index.js
@@ -0,0 +1,9 @@
+var name = 'Joe';
+var i = 0;
+
+while (i < 2) {
+	i = i + 1;
+	document.write('Happy birthday to you. <br>');
+}
+document.write('Happy birthday dear ' + name + ', <br>');
+document.write('Happy birthday to you. <br>');
