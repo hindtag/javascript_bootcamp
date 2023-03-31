ef128c5d1405e563c6b39e769e609491708f8e2d
hindtag <mbnvargas>
2023-03-31 18:04:05 +0800

    JS ebook page 69

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var name = 'Joe';
-var i = 0;
+let word = 'bottles';
+let count = 99;
 
-while (i < 2) {
-	i = i + 1;
-	document.write('Happy birthday to you. <br>');
+while (count > 0) {
+	console.log(count + ' ' + word + ' of beer on the wall');
+	console.log(count + ' ' + word + ' of beer');
+	console.log('Take one down, pass it around');
+	count -= 1;
 }
-document.write('Happy birthday dear ' + name + ', <br>');
-document.write('Happy birthday to you. <br>');
