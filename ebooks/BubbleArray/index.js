96bdb5b8f8994c3ca30b5f2e1c9e6174a38932e5
hindtag <mbnvargas>
2023-04-05 03:08:07 +0800

    Studying objects

--- ebooks/BubbleArray/index.js
+++ ebooks/BubbleArray/index.js
@@ -3,8 +3,11 @@ let scores = [
 	54, 66, 55, 52, 61, 46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44,
 ];
 
-scores.push(100); // Pushing new score at the end of an array
-scores.push(100); // Pushing new score at the end of an array
+let costs = [
+	0.25, 0.27, 0.25, 0.25, 0.25, 0.25, 0.33, 0.31, 0.25, 0.29, 0.27, 0.22, 0.31,
+	0.25, 0.28, 0.25, 0.24, 0.22, 0.2, 0.25, 0.3, 0.25, 0.24, 0.25, 0.25, 0.25,
+	0.27, 0.25, 0.26, 0.29,
+];
 
 function printAndGetHighScore(score) {
 	let highScore = 0;
@@ -36,3 +39,25 @@ function getBestResults(scores, highScore) {
 
 let bestSolution = getBestResults(scores, highScore);
 console.log('solution with the highest score: ' + bestSolution);
+
+// Cost effective solution
+
+function getMostEffectiveSolution(scores, costs, highScore) {
+	let cost = 100;
+	let index;
+
+	for (let i = 0; i < scores.length; i++) {
+		if (scores[i] == highScore) {
+			if (cost > costs[i]) {
+				index = i;
+				cost = costs[i];
+			}
+		}
+	}
+	return index;
+}
+
+let MostCostEffective = getMostEffectiveSolution(scores, costs, highScore);
+console.log(
+	'Bubble Solution #' + MostCostEffective + ' is the most cost effective'
+);
